ecd2cc76db5251bdf0834204085898a389e67840
Carlo Ortega <cfor0@outlook.com>
2019-11-26 09:23:56 -0500

    react boiler plate

---
+++ clicky-game/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ClickCard from "./components/ClickCard";
+import Title from "./components/Title"
+
+
+class App extends Components {
+  state = {
+    picture
+  }
+  
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default App;
