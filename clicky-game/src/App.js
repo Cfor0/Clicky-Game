fff96bf65199df64ea0dd166c891692b2db21958
Carlo Ortega <cfor0@outlook.com>
2019-11-26 10:50:41 -0500

    rendering images to app

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ClickCard from "./components/ClickCard";
 import Title from "./components/Title"
-import Wrapper from "./components/Wrapper"
+import Wrapper from "./components/Wrapper";
+import images from "./images.json"
 
 
-class App extends Components {
+class App extends Component {
   state = {
-    pictures
+    images
   }
   switchPictures = id => {
-
+    const images = this.state.images.filter(image => image.id !== id);
+    this.setState({ images })
   }
   render() {
     return (
       <Wrapper>
         <Title>Clicky Game</Title>
-        <ClickCard>
+        {this.state.images.map(image => (
+          <ClickCard
+            id={image.id}
+            key={image.id}
+            name={image.name}
+            image={image.image}
+          />
+        ))}
 
-        </ClickCard>
       </Wrapper>
 
     )
