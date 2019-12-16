f121ed36c4d95e3c0e91e500ab052ab3f2b10865
Carlo Ortega <cfor0@outlook.com>
2019-12-16 18:44:17 -0500

    formatted images

--- src/components/ClickCard.js
+++ src/components/ClickCard.js
@@ -3,9 +3,11 @@ import "./style.css"
 
 function ClickCard(props) {
     return (
-        <div className="card">
-            <div className="img-container">
-                <img alt={props.name} src={props.image} onClick={() => props.click(props.id)} className="remove"></img>/>
+        <div className="container">
+            <div className="card">
+                <div className="img-container">
+                    <img alt={props.name} src={props.image} onClick={() => props.click(props.id)} className="remove"></img>/>
+            </div>
             </div>
         </div>
     )
